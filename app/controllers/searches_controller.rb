b34c1ff977bf24ce960d1181c08344d637a005ec
patrobas <patrobasbwire@gmail.com>
2024-01-21 16:17:46 +0300

    made some configurations on application.rb and searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,53 +1,95 @@
+# require 'trie'
+
+# class SearchesController < ApplicationController
+#   before_action :initialize_search_analytics
+
+#   def index
+#     query = params[:query]
+#     if query.present?
+#       user_ip = request.headers['X-Forwarded-For'] || request.remote_ip
+#       Search.create(query: query, user_ip: user_ip)
+#     end
+
+#     @searches = Search.order(created_at: :desc).limit(20)
+#     render json: @searches
+#   end
+
+
+#   def create
+#     query = params[:query]
+#     user_ip = request.headers['X-Forwarded-For'] || request.remote_ip
+
+#     if query.present?
+#       last_complete_query = @search_analytics.get_last_complete_query(query)
+
+#       if last_complete_query.present?
+#         Search.create(query: last_complete_query, user_ip: user_ip)
+#         render json: { status: 'success', message: 'Search recorded successfully' }, status: :created
+#       else
+#         render json: { status: 'error', message: 'Incomplete search query' }, status: :unprocessable_entity
+#       end
+#     else
+#       render json: { status: 'error', message: 'Empty search query' }, status: :unprocessable_entity
+#     end
+#   end
+
+#   private
+
+#   def initialize_search_analytics
+#     @search_analytics = SearchAnalytics.new
+#   end
+# end
+
+# app/controllers/searches_controller.rb
+
 class SearchesController < ApplicationController
-  before_action :initialize_searches
+  before_action :initialize_search_analytics
 
- def index
-  query = params[:query]
-  if query.present?
-    user_ip = request.headers['X-Forwarded-For'] || request.remote_ip
-    if complete_and_longest_search?(query)
-      Search.create(query: query, user_ip: user_ip)
-    else
-      Rails.logger.debug("Not saving search: #{query}")
-    end
-  end
+  # def index
+  #   query = params[:query]
+  #   if query.present?
+  #     user_ip = request.headers['X-Forwarded-For'] || request.remote_ip
+  #     last_complete_query = @search_analytics.get_last_complete_query(query)
 
-  @searches = Search.order(created_at: :desc).limit(20)
-  render json: @searches
-end
+  #     if last_complete_query.present?
+  #       Search.create(query: last_complete_query, user_ip: user_ip)
+  #     else
+  #       Rails.logger.debug("Not saving search: #{query}")
+  #     end
+  #   end
 
+  #   @searches = Search.order(created_at: :desc).limit(20)
+  #   render json: @searches
+  # end
 
-  def create
+  def index
     query = params[:query]
-    user_ip = request.headers['X-Forwarded-For'] || request.remote_ip
+    search_analytics = SearchAnalytics.new
 
     if query.present?
-      Search.create(query: query, user_ip: user_ip) if complete_and_longest_search?(query)
-      render json: { status: 'success', message: 'Search recorded successfully' }, status: :created
-    else
-      render json: { status: 'error', message: 'Empty search query' }, status: :unprocessable_entity
-    end
-  end
+      last_complete_query = search_analytics.get_last_complete_query(query)
 
+      if last_complete_query.nil? || last_complete_query.strip != query.strip
+        # Save the query only if it's a new and complete query
+        Search.create(query: query, user_ip: request.remote_ip)
+      end
+    end
+
+    # Fetch recent searches based on creation timestamp
+    @searches = Search.order(created_at: :desc).limit(20)
+
+    render json: @searches
+  end
+    
   private
 
-  def initialize_searches
-    @search_queries = Search.order(created_at: :desc).limit(20).pluck(:query)
+  def initialize_search_analytics
+    @search_analytics = SearchAnalytics.new
   end
-
-  def complete_and_longest_search?(query)
-    existing_longer = @search_queries.any? { |existing_query| query.downcase.include?(existing_query.downcase) && query.length > existing_query.length }
-
-    if existing_longer
-      true
-    else
-      @search_queries.reject! { |existing_query| query.downcase.include?(existing_query.downcase) && query.length > existing_query.length }
-      @search_queries << query
-      false
-    end
-  end
-  
 end
+
+
+
 
 
 
---
+++ lib/search_analytics.rb
@@ -0,0 +1,88 @@
+# lib/search_analytics.rb
+# require 'set'
+
+# class SearchAnalytics
+#   def initialize
+#     @recorded_queries = Set.new
+#   end
+
+#   def get_last_complete_query(query)
+#     current_length = query.split.size
+
+#     if current_length > 1 && !@recorded_queries.include?(query)
+#       @recorded_queries.add(query)
+#       return query.strip
+#     end
+
+#     @last_complete_query ||= ''
+#     if @last_complete_query.split.size < current_length
+#       @last_complete_query = query
+#     end
+
+#     @last_complete_query.strip
+#   end
+# end
+
+# lib/search_analytics.rb
+
+# require 'set'
+
+# class SearchAnalytics
+#   def initialize
+#     @recorded_queries = Set.new
+#   end
+
+#   def get_last_complete_query(query)
+#     query_words = query.split
+#     current_length = query_words.size
+
+#     # Check if the query is complete and not previously recorded
+#     if current_length > 1 && !@recorded_queries.include?(query)
+#       @recorded_queries.add(query)
+#       return query.strip
+#     end
+
+#     # If the current query is longer than the last recorded one, update it
+#     @last_complete_query ||= ''
+#     if @last_complete_query.split.size < current_length
+#       @last_complete_query = query
+#     end
+
+#     @last_complete_query.strip
+#   end
+# end
+
+# lib/search_analytics.rb
+
+# lib/search_analytics.rb
+
+require 'set'
+
+class SearchAnalytics
+  def initialize
+    @recorded_queries = Set.new
+  end
+
+  def get_last_complete_query(query)
+    node = @root
+    last_complete_query = ''
+  
+    query.split.each do |word|
+      return nil unless node&.children
+      node = node.children[word] if node.children.key?(word)
+      last_complete_query += "#{word} " if node&.is_end_of_word
+    end
+  
+    # Check if the last complete query is the same as the input query
+    last_complete_query.strip == query.strip ? last_complete_query : nil
+  end
+  
+
+  private
+
+  def similar_query_exists?(query)
+    # Check if a similar complete query is already in the recorded queries
+    @recorded_queries.any? { |recorded_query| recorded_query.include?(query) }
+  end
+end
+
